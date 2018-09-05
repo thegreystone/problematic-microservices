e9a9a06c5c2a1f0847a64280b50c4ec71ea93ade
Marcus Hirt <marcus@hirt.se>
2018-09-05 21:09:05 +0200

    Robot factory updates

---
+++ robot-order-service/src/main/java/se/hirt/examples/robotfactory/ResourceLoader.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018 Marcus Hirt
+ *                    www.hirt.se
+ *
+ * This software is free:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESSED OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright (C) Marcus Hirt, 2018
+ */
+package se.hirt.examples.robotfactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.jsonb.internal.JsonBindingProvider;
+
+import se.hirt.examples.robotfactory.rest.RobotTypeResource;
+import se.hirt.examples.robotfactory.rest.RobotTypesResource;
+import se.hirt.examples.robotfactory.rest.FactoryResource;
+import se.hirt.examples.robotfactory.rest.HelloRest;
+
+/**
+ * Programmatic adding of our specific REST resources.
+ * 
+ * @author Marcus Hirt
+ */
+public class ResourceLoader extends Application {
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		final Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(HelloRest.class);
+		classes.add(RobotTypeResource.class);
+		classes.add(RobotTypesResource.class);
+		classes.add(FactoryResource.class);
+		classes.add(JsonBindingProvider.class);
+		return classes;
+	}
+}
---
+++ robot-order-service/src/main/java/se/hirt/examples/robotfactory/data/Color.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2018 Marcus Hirt
+ */
+package se.hirt.examples.robotfactory.data;
+
+/**
+ * Just a color.
+ * 
+ * @author Marcus Hirt
+ */
+public enum Color {
+	RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, BLACK, WHITE, PINK
+}
